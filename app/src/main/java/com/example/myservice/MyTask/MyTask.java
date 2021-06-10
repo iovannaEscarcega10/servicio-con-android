59f5a3432500e836eab658a0a675c5558db069f8
Iovanna Denisse Escarcega Soto <iovassoto@gmail.com>
2021-06-10 10:32:47 -0500

    cambios en main activity y agregar tarea

---
+++ app/src/main/java/com/example/myservice/MyTask/MyTask.java
@@ -0,0 +1,10 @@
+package com.example.myservice.MyTask;
+
+import android.os.AsyncTask;
+
+public class MyTask extends AsyncTask<String, String, String> {
+    @Override
+    protected String doInBackground(String... strings) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/myservice/Services/MyService.java
+++ app/src/main/java/com/example/myservice/Services/MyService.java
@@ -3,6 +3,7 @@ package com.example.myservice.Services;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class MyService extends Service {
     public MyService() {
@@ -12,5 +13,18 @@ public class MyService extends Service {
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+    public void onCreate(){
+        super.onCreate();
+        Toast.makeText(this, "SE INICIÓ EL SERVICIO", Toast.LENGTH_SHORT).show();
+    }
+    public void onDestroy(){
+        super.onDestroy();
+        Toast.makeText(this, "SE DETUVO EL SERVICIO", Toast.LENGTH_SHORT).show();
     }
 }
